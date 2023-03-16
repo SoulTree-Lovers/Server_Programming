f9b2dece3a6cb96968964332cc72e2d30f88269b
강승민 <rkdtmdals999@naver.com>
2023-03-16 12:53:59 +0900

    Add annotation

--- 과제 1/webprogram_1_KangSeungmin.py
+++ 과제 1/webprogram_1_KangSeungmin.py
@@ -1,6 +1,7 @@
 # 1. 사용자로 부터 comma seprated 입력 문자열을 받아서, list와 tuple을 만들어 출력하시오
 
 #  coding here
+# split(',')를 통해 comma 구분자로 문자열을 나누어 입력받는다.
 string = input("Input some comma separated numbers : ").split(',')
 comma_separated_string_to_list = list(string)
 comma_separated_string_to_tuple = tuple(string)
@@ -18,6 +19,7 @@ print(comma_separated_string_to_tuple)
 #    예를 들면, 입력숫자가 5인 경우 5+55+555로 계산되어 615값을 출력함
 
 #  coding here
+# 문자열로 입력 받은 후, 문자열 덧셈 연산 이후 int() 함수를 통해 수로 바꾸어 계산한다.
 n = input("Input an integer : ")
 n1 = int(n)
 n2 = int(n+n)
@@ -44,6 +46,8 @@ numbers = [
 
 
 #  coding here
+# numbers를 반복하며 237 발견 시 break한다.
+# num을 2로 나눈 나머지가 0일 때(짝수인 경우) 출력한다.
 for num in numbers:
     if num == 237:
         break
@@ -72,6 +76,7 @@ for num in numbers:
 # input string
 def char_frequency(str1):
     # coding here (pass 명령어를 제거하고 coding 할 것)
+    # 딕셔너리를 만들고 처음 입력받는 값이라면 1로 설정, 그 이후는 += 1을 한다.
     char_dict = dict()
 
     for i in str1:
@@ -97,6 +102,7 @@ my_list = [10, 20, 30, 40, 20, 50, 60, 40]
 
 
 #  coding here
+# set()으로 중복 원소를 제거하고 list()로 다시 변환한다.
 print(list(set(my_list)))
 
 # Console Output
@@ -108,6 +114,7 @@ print(list(set(my_list)))
 # 6. 다음 input dictionary 변수들을 합쳐서 새로운 dictionary로 만든 후 출력하시오
 
 #  coding here
+# update()를 통해 딕셔너리를 병합한다.
 dic1={1:10, 2:20}
 dic2={3:30, 4:40}
 dic3={5:50,6:60}
@@ -127,6 +134,7 @@ print(dic1)
 
 
 #  coding here
+# 딕셔너리를 만들어 입력한다.
 num_dic = dict()
 
 n = int(input("Input an integer : "))
@@ -146,6 +154,7 @@ print(num_dic)
 
 
 #  coding here
+# join()을 통해 빈 문자열을 튜플의 원소 사이사이에 삽입하여 문자열을 생성한다.
 tup = ('e', 'x', 'e', 'r', 'c', 'i', 's', 'e', 's')
 
 print("".join(tup))
@@ -160,6 +169,7 @@ print("".join(tup))
 
 
 #  coding here
+# 합집합을 통해 계산한다.
 setx = set(["green", "blue"])
 sety = set(["blue", "yellow"])
 
@@ -181,6 +191,7 @@ input_list = [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
 
 def sort_list_last(tuples):
     # coding here (pass 명령어를 제거하고 coding 할 것)
+    # lambda()함수를 통해 각 리스트 원소의 1번째 원소를 기준으로 정렬한다.
     return sorted(tuples, key=lambda x : x[1])
 
 print(sort_list_last(input_list))
